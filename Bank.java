ae89ac8daccb8b8ef6c08607a9e549dbc415296f
Jegathese <imjegathese04@gmail.com>
2021-11-02 05:18:20 +0530

    first program

---
+++ Bank.java
@@ -0,0 +1,40 @@
+class Bank
+{
+public static void main(String[] args)
+{
+Bank manager = new Bank();
+double balance = manager.calculateInterest(100000);
+	System.out.println(balance);
+	balance = manager.calculateInterest(100000,5);
+	
+System.out.println(balance);
+balance = manager.calculateInterest(1000,12,6.0f);	
+	System.out.println(balance);
+double balance = manager.calculateInterest(100000);
+	String double balance = manager.calculateInterest(100000);
+customer = manager.calculateInterest(10000,"Kathir");
+	System.out.println(customer);
+}
+	public String calculateInterest(int amt, String name)
+	{
+	return name;
+	}
+public double calculateInterest(int amount)
+{
+float interest = 4.5f;
+double value = amount*4.5/100;
+return value;
+}
+
+public double calculateInterest(int amount,int year)
+{
+float interest = 4.5f;
+double value = amount*4.5/100*year;
+return value;
+}
+	public double calculateInterest(int p,int n,float r)
+{
+return p*n*(r/100);
+}
+
+}
